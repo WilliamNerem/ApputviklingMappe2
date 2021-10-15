bcb73732c9e85d881081142f2b620bf85436e3f1
WilliamNerem <william-kn@hotmail.com>
2021-10-15 12:25:31 +0200

    added icons

--- ApputviklingMappe2/app/src/main/java/com/example/apputviklingmappe2/MainActivity.java
+++ ApputviklingMappe2/app/src/main/java/com/example/apputviklingmappe2/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         toolbar=findViewById(R.id.app_bar);
-
         setSupportActionBar(toolbar);
+
     }
 }
