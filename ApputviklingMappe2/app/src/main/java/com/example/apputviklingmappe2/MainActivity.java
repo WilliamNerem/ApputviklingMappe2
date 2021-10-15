d9338e553fb0c9e89f5eff6fac892f3aa801344d
WilliamNerem <william-kn@hotmail.com>
2021-10-15 11:20:42 +0200

    Added top_bar

--- ApputviklingMappe2/app/src/main/java/com/example/apputviklingmappe2/MainActivity.java
+++ ApputviklingMappe2/app/src/main/java/com/example/apputviklingmappe2/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.apputviklingmappe2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar=findViewById(R.id.app_bar);
+
+        setSupportActionBar(toolbar);
     }
 }
