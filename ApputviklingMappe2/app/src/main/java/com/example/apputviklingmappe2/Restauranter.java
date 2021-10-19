50aa1d756088fce6bf02f78f9dc9839b75ced4ff
WilliamNerem <william-kn@hotmail.com>
2021-10-19 11:16:09 +0200

    Lagt til restauranter page

--- ApputviklingMappe2/app/src/main/java/com/example/apputviklingmappe2/Restauranter.java
+++ ApputviklingMappe2/app/src/main/java/com/example/apputviklingmappe2/Restauranter.java
@@ -1,6 +1,10 @@
 package com.example.apputviklingmappe2;
 
 import android.os.Bundle;
+import android.view.ViewTreeObserver;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -9,9 +13,20 @@ import java.util.Objects;
 
 public class Restauranter extends AppCompatActivity {
 
+    private Spinner spinner;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.restauranter);
+        spinner = (Spinner) findViewById(R.id.restaurantType);
+        setSpinner();
+    }
+
+    private void setSpinner() {
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.RestaurantTypes, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
